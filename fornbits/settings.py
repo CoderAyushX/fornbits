289e687ed05831112fdc5d850a0d86c8560a4e60
Ayush kumar <ayushris49271@gmail.com>
2022-01-31 20:46:11 +0530

    first commit

--- fornbits/settings.py
+++ fornbits/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=&z9*+sgqxc$a_i%p=&j!xu1&0mblgdu3pwnmwjkvj+7fv0l)u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '*'
