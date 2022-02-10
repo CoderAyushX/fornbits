fef5ac055caa574fbcf08e4818aad5ea7a9aa8b2
Ayush kumar <ayushris49271@gmail.com>
2022-02-10 20:34:48 +0530

    first commit

--- static/main.js
+++ static/main.js
@@ -16,5 +16,6 @@ function delay (URL) {
 }
 window.addEventListener("load", function() {
   document.getElementById("loader").style.display = "none";
+  document.getElementById("body").style.display = "";
 });
 
