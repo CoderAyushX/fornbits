ae96f1c07e59de4df5fab9939c9c6bedb8fc444b
Ayush kumar <ayushris49271@gmail.com>
2022-02-10 20:09:16 +0530

    first commit

--- static/main.js
+++ static/main.js
@@ -12,6 +12,6 @@ function activeLink() {
 list.forEach((item)=> item.addEventListener( 'click', activeLink ))
 
 function delay (URL) {
-  setTimeout( function() { window.location = URL }, 500 );
+  setTimeout( function() { window.location = URL }, 200 );
 }
 
