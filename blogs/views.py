bc2b6a45984d6511ed117f1f455590d51be8b3ab
Ayush kumar <ayushris49271@gmail.com>
2022-02-12 21:56:48 +0530

    first commit

--- blogs/views.py
+++ blogs/views.py
@@ -1,50 +1,54 @@
-from django.shortcuts import render,redirect
-from .models import Category,Post,blogpostComment
+from django.shortcuts import render, redirect
+from .models import Category, Post, blogpostComment
 
 # Create your views here.
+
+
 def blogs(request):
     cat = Category.objects.all()
     post = Post.objects.all()
     context = {
         'cats': cat,
-        'posts':post
+        'posts': post
     }
     return render(request, 'blogs.html', context)
 
+
 def blogpost(request, url):
     try:
-      post =  Post.objects.get(url=url)
-      relatedPost = Post.objects.all().exclude(post_id= post.post_id)[:5]
-    #work to do here
-      comments = blogpostComment.objects.filter (post = post)
-      context = {
-        'posts':post,
-        'comment': comments,
-        'relatedpost': relatedPost
-      }
-      return render(request, 'blogpost.html', context)
+        post = Post.objects.get(url=url)
+        relatedPost = Post.objects.all().exclude(post_id=post.post_id)[:5]
+        # work to do here
+        comments = blogpostComment.objects.filter(post=post)
+        context = {
+            'posts': post,
+            'comment': comments,
+            'relatedpost': relatedPost
+        }
+        return render(request, 'blogpost.html', context)
     except:
         return render(request, '500.html', context)
 
 
 def category(request, url):
-    searchCat = Category.objects.get(url = url)
-    post =  Post.objects.filter(cat=searchCat)
+    searchCat = Category.objects.get(url=url)
+    post = Post.objects.filter(cat=searchCat)
     cat = Category.objects.all()
     context = {
         'cats': cat,
-        'posts':post
+        'posts': post
     }
     return render(request, 'blogs.html', context)
+
 
 def comment(request):
     if request.user.is_anonymous:
         return redirect('login')
     if request.method == "POST":
-         comment = request.POST.get('comment')
-         user = request.user
-         postno  =  request.POST.get('Sno')
-         post = Post.objects.get(post_id = postno)
-         comment = blogpostComment(comments= comment, user= user, post= post)
-         comment.save()
+        comment = request.POST.get('comment')
+        user = request.user
+        postno = request.POST.get('Sno')
+        post = Post.objects.get(post_id=postno)
+        comment = blogpostComment(comments=comment, user=user, post=post)
+        comment.save()
     return redirect(f'/blogs/{post.url}')
