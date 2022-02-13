2ba20cd4ea14542a3a351aa97d75b91553754d0b
Ayush kumar <ayushris49271@gmail.com>
2022-02-13 11:12:35 +0530

    first commit

--- blogs/models.py
+++ blogs/models.py
@@ -3,7 +3,11 @@ from django.utils.timezone import now
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 # Create your models here.
+CONTENT_TYPE = (
+    ("long", "Long"),
+    ("short", "Short"),
 
+)
 #category modle
 class Category(models.Model):
     cat_id = models.AutoField(primary_key=True)
@@ -24,11 +28,15 @@ class Post(models.Model):
     description = models.TextField()
     content = HTMLField()
     url = models.CharField( max_length=150)
-    cType = models.CharField(max_length=50)
+    cType = models.CharField(
+        max_length = 20,
+        choices = CONTENT_TYPE,
+        default = 'long'
+        )
     cat = models.ForeignKey(Category, on_delete=models.CASCADE)
     image = models.URLField(max_length=350)
     upload_time = models.DateTimeField(default= now)
-
+    tags = models.TextField()
     def __str__(self):
         return self.title
 
