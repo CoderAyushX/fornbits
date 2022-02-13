2bf552ba1ea75d2259a785ccd417cd9c2ae52724
Ayush kumar <ayushris49271@gmail.com>
2022-02-13 12:50:20 +0530

    first commit

--- static/main.js
+++ static/main.js
@@ -27,8 +27,8 @@ if (localStorage.getItem("theme") == false || localStorage.getItem("theme") == "
   r.style.setProperty('--pText', 'black');
   r.style.setProperty('--subject', '#1C6DD0' )
   document.getElementById("blogpost").style.boxShadow = "rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px";
-  document.getElementById("content").style.boxShadow = "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px";
+  document.querySelector(".content").style.boxShadow = "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px";
 }else if(localStorage.getItem("theme") == true || localStorage.getItem("theme") == "true"){
   document.getElementById("blogpost").style.boxShadow = "none";
-  document.getElementById("content").style.boxShadow = "rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px";
+  document.querySelector(".content").style.boxShadow = "rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px";
 }
